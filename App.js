66df0e856b24c0af56218b2541d93b49bd67d170
enigma-4 <92198089+enigma-4@users.noreply.github.com>
2022-02-17 12:27:46 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import { Table } from 'antd';
+import 'antd/dist/antd.min.css';
+import { columns, data } from './dataSource';
+
+function App() {
+  return <Table dataSource={data} columns={columns} pagination={false} />;
+}
+
+export default App;
